b7aece235be9c431a579af84840605eb4fff4594
Ben Danon <danon.bendavid@post.idc.ac.il>
2020-03-23 08:02:18 +0200

    added template

---
+++ template.py
@@ -0,0 +1,41 @@
+import unittest
+import math
+
+"""
+This is a template
+"""
+
+
+def solution(A):
+    return 0
+
+
+class TestTemplate(unittest.TestCase):
+
+    def test_basic(self):
+        A = []
+        self.assertEqual(solution(A), 0)
+
+    def test_trivial(self):
+        A = []
+        self.assertEqual(solution(A), 0)
+
+    def test_example(self):
+        A = []
+        self.assertEqual(solution(A), 0)
+
+    def test_negative(self):
+        A = []
+        self.assertEqual(solution(A), 0)
+
+    def test_identical(self):
+        A = []
+        self.assertEqual(solution(A), 0)
+
+    def test_challenging(self):
+        A = []
+        self.assertEqual(solution(A), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
