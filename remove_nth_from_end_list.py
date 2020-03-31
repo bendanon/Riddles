06770e429e08a333a9e09806d08db2e9278ec955
Ben Danon <danon.bendavid@post.idc.ac.il>
2020-03-31 19:49:23 +0300

    solved remove nth node from end and fixed the from_list static method in ListNode class

---
+++ remove_nth_from_end_list.py
@@ -0,0 +1,66 @@
+import unittest
+from Algorithms.list import ListNode
+
+"""
+Given a linked list, remove the nth node from the end of list and return its head.
+"""
+
+
+def solution(A, B):
+
+    back = A
+
+    if back is None:
+        return None
+
+    front = back
+
+    # create a lead of B nodes from back to front
+    while front is not None and B > 0:
+        front = front.next
+        B -= 1
+
+    # This means B is larger than the length of the list
+    # Remove the first node
+    if front is None:
+        return back.next
+
+    while front.next is not None:
+        front = front.next
+        back = back.next
+
+    back.next = back.next.next
+
+    return A
+
+
+class TestReverseList(unittest.TestCase):
+
+    def test_basic(self):
+        A = [1, 2, 3]
+        B = 1
+        self.assertEqual([1, 2], solution(ListNode.from_list(A), B))
+
+    def test_trivial_none(self):
+        A = []
+        B = 1
+        self.assertEqual(None, solution(ListNode.from_list(A), B))
+
+    def test_trivial_one(self):
+        A = [1]
+        B = 1
+        self.assertEqual(None, solution(ListNode.from_list(A), B))
+
+    def test_trivial_two(self):
+        A = [1, 2]
+        B = 2
+        self.assertEqual([2], solution(ListNode.from_list(A), B))
+
+    def test_example(self):
+        A = [1, 2, 3, 4, 5]
+        B = 2
+        self.assertEqual([1, 2, 3, 5], solution(ListNode.from_list(A), B))
+
+
+if __name__ == '__main__':
+    unittest.main()
