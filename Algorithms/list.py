ed367fc7f17d48f4c2072068548a9e31b1b90f1d
Ben Danon <danon.bendavid@post.idc.ac.il>
2020-03-31 19:29:29 +0300

    Added a singly linked list implementation and solved reverse list

---
+++ Algorithms/list.py
@@ -0,0 +1,31 @@
+class ListNode:
+
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+    def __eq__(self, other):
+
+        if not isinstance(other, list):
+            return False
+
+        node = self
+        for i in other:
+            if i != node.val:
+                return False
+            node = node.next
+
+        return True
+
+    @staticmethod
+    def from_list(lst):
+        if len(lst) == 0:
+            return
+
+        head = ListNode(lst[0])
+        n = head
+        for i in lst:
+            n.next = ListNode(i)
+            n = n.next
+
+        return head
---
+++ reverse_list.py
@@ -0,0 +1,50 @@
+import unittest
+from Algorithms.list import ListNode
+
+"""
+Reverse a linked list. Do it in-place and in one-pass.
+"""
+
+def solution(A):
+
+    prev = A
+    if prev is None:
+        return None
+
+    current = prev.next
+    prev.next = None
+
+    while current is not None:
+        next = current.next
+        current.next = prev
+        prev = current
+        current = next
+
+    return prev
+
+
+class TestReverseList(unittest.TestCase):
+
+    def test_basic(self):
+        A = [1, 2, 3]
+        self.assertEqual([3, 2, 1], solution(ListNode.from_list(A)))
+
+    def test_trivial_none(self):
+        A = []
+        self.assertEqual(None, solution(ListNode.from_list(A)))
+
+    def test_trivial_one(self):
+        A = [1]
+        self.assertEqual([1], solution(ListNode.from_list(A)))
+
+    def test_trivial_two(self):
+        A = [1, 2]
+        self.assertEqual([2, 1], solution(ListNode.from_list(A)))
+
+    def test_example(self):
+        A = [1, 2, 3, 4, 5]
+        self.assertEqual([5, 4, 3, 2, 1], solution(ListNode.from_list(A)))
+
+
+if __name__ == '__main__':
+    unittest.main()
