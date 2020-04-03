f398dd008dee357896204821bb873c1b6880c4c2
Ben Danon <danon.bendavid@post.idc.ac.il>
2020-04-03 13:25:41 +0300

    made list prettier

--- python/Algorithms/list.py
+++ python/Algorithms/list.py
@@ -22,10 +22,10 @@ class ListNode:
         if len(lst) == 0:
             return
 
-        head = ListNode(lst[0])
+        head = ListNode(0)
         n = head
-        for i in lst[1:]:
+        for i in lst:
             n.next = ListNode(i)
             n = n.next
 
-        return head
+        return head.next
